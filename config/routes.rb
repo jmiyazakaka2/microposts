978bfb5c8ea9f9d27a3c5d588e390f642cb7dcdd
jmiyazakaka2 <jmiyazakaka2@gmail.com>
2016-05-03 06:10:51 +0000

    challenge:user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'signup', to: 'users#new'
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get    'edit'  , to: 'users#edit'
+  patch  'update', to: 'users#update'
+  put    'update', to: 'users#update'
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
